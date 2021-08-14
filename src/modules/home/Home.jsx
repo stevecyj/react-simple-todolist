f52f0ce8c64ded950f12ad5c80252c2eba59d193
root <root@localhost.localdomain>
2021-08-14 17:29:16 +0800

    chore:component,Home

---
+++ src/modules/home/Home.jsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <header>Home</header>;
+}
--- src/modules/todo-list/TodoList.jsx
+++ src/modules/todo-list/TodoList.jsx
@@ -24,7 +24,7 @@ export function TodoList() {
   }, []);
 
   return (
-    <header className="App-header">
+    <header>
       {todos.length === 0 && <div>No Data</div>}
       <ul>
         {todos.map((todo, idx) => (
