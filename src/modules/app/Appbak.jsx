3a37e7768cef7a652f540af360db488326d250d4
root <root@localhost.localdomain>
2021-08-14 17:38:13 +0800

    chore:Appbak

---
+++ src/modules/app/Appbak.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import './App.css';
+import { TodoList } from '../todo-list';
+import { Home } from '../home';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/todos">Todos</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/todos">
+              <TodoList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
