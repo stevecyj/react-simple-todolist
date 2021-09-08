95c2cdad561ffe667a4538d25c145c0dc22a0af4
Steve <ycsteven.tsao@gmail.com>
2021-09-09 01:07:15 +0800

    chore:move layouts to router

---
+++ src/modules/router/listItems.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+// import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+// import PeopleIcon from '@material-ui/icons/People';
+// import BarChartIcon from '@material-ui/icons/BarChart';
+// import LayersIcon from '@material-ui/icons/Layers';
+// import AssignmentIcon from '@material-ui/icons/Assignment';
+import { Link as RouterLink } from 'react-router-dom';
+// import { default as MaterialLink } from '@material-ui/core/Link';
+
+export const mainListItems = (
+  <div>
+    {/* <MaterialLink component={RouterLink} to="/" /> */}
+    <ListItem button component={RouterLink} to="/login">
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Login" />
+    </ListItem>
+    <ListItem button component={RouterLink} to="/">
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItem>
+    {/* <MaterialLink component={RouterLink} to="/todos" /> */}
+    <ListItem button component={RouterLink} to="/todos">
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Todos" />
+    </ListItem>
+    <ListItem button component={RouterLink} to="/ticket-list">
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="TicketList" />
+    </ListItem>
+    <ListItem button component={RouterLink} to="/incidences">
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Incidences" />
+    </ListItem>
+    {/* <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Customers" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Integrations" />
+    </ListItem> */}
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    {/* <ListSubheader inset>Saved reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItem> */}
+  </div>
+);
